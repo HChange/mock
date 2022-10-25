45fa1dc40035e9a691fa0a7a95b67c4cac6d7ec6
Change <chensj@hua-cloud.com.cn>
2022-10-25 16:20:27 +0800

    feat: 文档优化

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,17 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: `Change's mock` });
 });
 
-router.get('/get', (req, res, next) => {
+router.get('/get', async (req, res, next) => {
   let data;
   const query = req.query;
-  data = getMock(query, res);
+  data = await getMock(query, res);
   res.send(data);
 });
 
-router.post('/get', (req, res, next) => {
+router.post('/get', async (req, res, next) => {
   let data;
   const query = req.body;
-  data = getMock(query, res);
+  data = await getMock(query, res);
   res.send(data);
 });
 
